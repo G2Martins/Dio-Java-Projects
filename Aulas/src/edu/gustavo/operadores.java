8f485599879f152313e58a513204b583d6e98a3c
G2Martins <gustavogripaldi@outlook.com>
2023-09-13 20:15:51 -0300

    docs: add operadores.java

---
+++ Aulas/src/edu/gustavo/operadores.java
@@ -0,0 +1,16 @@
+package edu.gustavo;
+/* 
++-----------------------------------------+ 
+ OperadorAtribuicoes:
+
+ String nome = "GLEYSON";
+ int idade = 22;
+ double peso = 68.5;
+ char sexo = 'M';
+ boolean doadorOrgao = false;
+ Date dataNascimento = new Date();
++-----------------------------------------+
+*/
+public class operadores {
+    
+}
