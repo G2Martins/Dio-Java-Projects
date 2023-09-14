e0eafb11ec74f475c3649143a79cdd28ad2a6f05
G2Martins <gustavogripaldi@outlook.com>
2023-09-14 20:02:23 -0300

    docs: add aulaMetodo_01.java

---
+++ Aulas/src/edu/aulaMetodo_01.java
@@ -0,0 +1,46 @@
+/*
+ public double somar(int num1, int num2){
+        // Logica --> Finalidade do Método
+        return ...;
+    } 
+ 
+    public void imprimir(String texto){
+        // Logica --> Finalidade do Método
+        // Aqui não precisa do Return
+        // Pois nao será retornado nenhum resultado
+ }
+    // Throws Exception: Indica que o Método a ser utilizado
+    // Poderá gerar uma exeção
+    public double dividir(int dividendo, int divisor) throws Exception{}
+
+    // Este Método não pode ser visto por outras clases no programa
+    private void metodoPrivado(){}
+
+    // Alguns Equívicos estruturais:
+    public void validar(){
+        // Este método deveria retornar algum valor
+        // no caso boolean (True or False)
+    }
+    
+    public void calcularEnviar(){
+        // Um Método deve representar uma única Responsabilidade
+    }
+
+    public void gravarCliente(String nome, String cpf, Integer telefone, ....){
+        //este método tem a finalidade de gravar
+		//informações de um cliente, por que não criar
+		//um objeto cliente e passar como parâmetro ?
+		//veja abaixo:
+    }
+
+    public void gravarCliente(Cliente cliente){}
+	//ou
+	public void gravar(Cliente cliente){}
+
+ */
+
+package edu;
+
+public class aulaMetodo_01 {
+    
+}
