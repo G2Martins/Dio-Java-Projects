bab6204cd53768418fde6c6a6e295bc3872a24f0
G2Martins <gustavogripaldi@outlook.com>
2023-09-14 20:32:06 -0300

    docs: rename minhaAula.java e exercicio smart TV

--- Aulas/src/SmartTV.java
+++ Aulas/src/SmartTV.java
@@ -36,11 +36,48 @@
     public void gravarCliente(Cliente cliente){}
 	//ou
 	public void gravar(Cliente cliente){}
-
  */
 
-package edu;
 
-public class aulaMetodo_01 {
+
+/*
+    Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+    1- Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+    2- Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+    3- Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+    4- Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente.
+ */
+public class SmartTV {
+    boolean ligada=false;
+    int canal=1;
+    int volume=25;
+
+    public void ligar(){
+        ligada=true;
+    }
     
+    public void desligar(){
+        ligada=false;
+    }
+    
+    public void aumentarVolume(){
+        volume++;
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+    }
+    
+    public void aumentarCanal(){
+        canal++;
+    }
+    
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
 }
--- Aulas/src/usuario.java
+++ Aulas/src/usuario.java
@@ -78,9 +78,29 @@ public class MinhaClasse
  */
 //+----------------------------------------------------------------------------------+
 
-
-public class minhaAula {
+public class usuario {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SmartTV smartTV = new SmartTV();
+        
+        System.out.println("TV Ligada ? " + smartTV.ligada);
+        System.out.println("Canal Atual ? " + smartTV.canal);
+        System.out.println("Volume Atual ? " + smartTV.volume);
+
+        smartTV.ligar();
+        System.out.println("Novo Status TV --> Ligada ? " + smartTV.ligada);
+        
+        smartTV.desligar();
+        System.out.println("Novo Status TV --> Ligada ? " + smartTV.ligada);
+
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        smartTV.diminuirVolume();
+        
+        smartTV.aumentarVolume();
+        System.out.println("Novo Status TV --> Volume Atual ? " + smartTV.volume); // 23
+
+        System.out.println("Status TV --> Canal Atual ? " + smartTV.canal);
+        smartTV.mudarCanal(13);
+        System.out.println("Novo Status TV --> Canal Atual ? " + smartTV.canal);
     }
 }
