1b7e4ab06dfad13053f2292583c2ffd599d2f1b1
G2Martins <gustavogripaldi@outlook.com>
2023-09-15 15:09:54 -0300

    docs: update ResultadoEscolar

--- Aulas_Estruturas_Condicionais/src/ResultadoEscolar.java
+++ Aulas_Estruturas_Condicionais/src/ResultadoEscolar.java
@@ -3,10 +3,13 @@ public class ResultadoEscolar {
 
        int nota = 6;
        
-       if(nota >= 7)
-        System.out.println("Aprovado");
-
-       else
-        System.out.println("Reprovado");
+        if(nota >= 7){
+            System.out.println("Aprovado");
+        }else if (nota >= 5 && nota < 7){
+            System.out.print("Prova de Recuperação");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
     }
 }
