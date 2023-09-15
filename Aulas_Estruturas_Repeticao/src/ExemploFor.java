fdeb249d5d8206d0dff17c2448fbb8c23e0dec59
G2Martins <gustavogripaldi@outlook.com>
2023-09-15 17:24:09 -0300

    docs: add ExemploFor.java file

---
+++ Aulas_Estruturas_Repeticao/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+	public static void main(String[] args) {
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+	}
+}
