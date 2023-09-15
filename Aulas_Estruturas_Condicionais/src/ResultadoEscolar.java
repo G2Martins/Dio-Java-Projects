b6282b8e5a37be0683650a2cb6f3828c0294313e
G2Martins <gustavogripaldi@outlook.com>
2023-09-15 13:45:40 -0300

    docs: add CaixaEletronico e ResultadoEscolar

---
+++ Aulas_Estruturas_Condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+       int nota = 6;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+       else
+        System.out.println("Reprovado");
+    }
+}
