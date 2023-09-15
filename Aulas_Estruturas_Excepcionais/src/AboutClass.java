e636a742aa58f569b074cec692eeea701801622a
G2Martins <gustavogripaldi@outlook.com>
2023-09-15 19:13:44 -0300

    docs: Add Aulas_Estruturas_Execepcionais folder

---
+++ Aulas_Estruturas_Excepcionais/src/AboutClass.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutClass 
+{   
+    public static void main(String[] args) 
+    {   
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        // Fechando o scanner explicitamente
+        scanner.close();
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();   
+    }
+}
