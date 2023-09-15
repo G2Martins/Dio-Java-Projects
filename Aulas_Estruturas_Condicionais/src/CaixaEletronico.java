e88659e8a1860a37d874963061878e39d791294f
G2Martins <gustavogripaldi@outlook.com>
2023-09-15 14:59:21 -0300

    docs: Update caixaEletronico file

--- Aulas_Estruturas_Condicionais/src/CaixaEletronico.java
+++ Aulas_Estruturas_Condicionais/src/CaixaEletronico.java
@@ -2,11 +2,18 @@ public class CaixaEletronico {
     public static void main(String[] args) {
 
        double saldo = 25.0;
-       double valorSolicitado = 17.0;
+       double valorSolicitado = 531.0;
 
-       if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
-
-        System.out.println(saldo);
+        if(valorSolicitado < saldo)
+        {
+            saldo = saldo - valorSolicitado;
+            System.out.println("\nTransação Concluída com Sucesso!\n");
+            System.out.println("\nSaldo Atual " + saldo );
+        }else
+            System.out.println("\nSaldo insuficiente, Transação cancelada");
+            System.out.println("\nSaldo Atual " + saldo );
+        
+        
+            
     }
 }
