86a222c4df59b87fc5c5376eacabd4aa11187177
G2Martins <gustavogripaldi@outlook.com>
2023-09-16 22:13:18 -0300

    docs: add JurosCompostos.java files

---
+++ Desafio_JurosCompostos/src/JurosCompostos.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class JurosCompostos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+   
+        double valorInicial = scanner.nextDouble();
+        
+        double taxaJuros = scanner.nextDouble();
+        
+        int periodo = scanner.nextInt();
+        
+        double valorFinal = calcularJurosCompostos(valorInicial, taxaJuros, periodo);
+
+        DecimalFormat df = new DecimalFormat("0.00");
+    
+        System.out.println("Valor final do investimento: R$ " + df.format(valorFinal));
+        
+        scanner.close();
+    }
+
+    public static double calcularJurosCompostos(double valorInicial, double taxaJuros, int periodo){
+        // Fórmula dos juros Compostos
+        double valorFinal = valorInicial * Math.pow(1 + taxaJuros, periodo);
+        // Arrendoda para duas casas decimais
+        return valorFinal;
+    }
+
+}
